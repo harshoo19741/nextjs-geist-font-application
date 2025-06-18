1d87e70bbe2ae07775d45fcb7475145d143a6778
harshoo19741 <harshoo19741@gmail.com>
2025-06-18 15:58:40 +0530

    Add .next/static/chunks/src_app_events_page_tsx_34437c69._.js 1750242519408

---
+++ .next/static/chunks/src_app_events_page_tsx_34437c69._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_events_page_tsx_781b74c3._.js"
+],
+    source: "dynamic"
+});
